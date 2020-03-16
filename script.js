98655b13424d513949e64f1e4656e35f34dd406c
Zorrita <lesyanaz96@gmail.com>
2020-03-16 22:39:56 +0700

    Two new descriptions added. Js functions for them created

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-function myFunction() {
-    var x = document.getElementById("fade-content").style;
+function showMore1() {
+    var x = document.getElementById("fade-content-1").style;
     if (x.maxHeight === "70px" || x.maxHeight === "") {
       x.maxHeight = "none";
-      document.getElementById('fade-overlay').style.display = "none";
-      document.getElementById('show-more-button').innerText = 'Свернуть';
+      document.getElementById('fade-overlay-1').style.display = "none";
+      document.getElementById('show-more-button-1').innerText = 'Свернуть';
     } else {
       x.maxHeight = "70px";
-      document.getElementById('fade-overlay').style.display = "block";
-      document.getElementById('show-more-button').innerText = 'Развернуть';
+      document.getElementById('fade-overlay-1').style.display = "block";
+      document.getElementById('show-more-button-1').innerText = 'Развернуть';
     }
   }
+
+function showMore2() {
+  var x = document.getElementById("fade-content-2").style;
+  if (x.maxHeight === "70px" || x.maxHeight === "") {
+    x.maxHeight = "none";
+    document.getElementById('fade-overlay-2').style.display = "none";
+    document.getElementById('show-more-button-2').innerText = 'Свернуть';
+  } else {
+    x.maxHeight = "70px";
+    document.getElementById('fade-overlay-2').style.display = "block";
+    document.getElementById('show-more-button-2').innerText = 'Развернуть';
+  }
+}
+
+function showMore3() {
+  var x = document.getElementById("fade-content-3").style;
+  if (x.maxHeight === "70px" || x.maxHeight === "") {
+    x.maxHeight = "none";
+    document.getElementById('fade-overlay-3').style.display = "none";
+    document.getElementById('show-more-button-3').innerText = 'Свернуть';
+  } else {
+    x.maxHeight = "70px";
+    document.getElementById('fade-overlay-3').style.display = "block";
+    document.getElementById('show-more-button-3').innerText = 'Развернуть';
+  }
+}
