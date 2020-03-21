8c1388fac2ac497768903e95b73af04f2834360a
Zorrita <lesyanaz96@gmail.com>
2020-03-21 14:03:24 +0700

    Max-height of descriptions to 65

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function showMore1() {
     var x = document.getElementById("fade-content-1").style;
-    if (x.maxHeight === "70px" || x.maxHeight === "") {
+    if (x.maxHeight === "65px" || x.maxHeight === "") {
       x.maxHeight = "none";
       document.getElementById('fade-overlay-1').style.display = "none";
       document.getElementById('show-more-button-1').innerText = 'Свернуть';
     } else {
-      x.maxHeight = "70px";
+      x.maxHeight = "65px";
       document.getElementById('fade-overlay-1').style.display = "block";
       document.getElementById('show-more-button-1').innerText = 'Развернуть';
     }
@@ -13,12 +13,12 @@ function showMore1() {
 
 function showMore2() {
   var x = document.getElementById("fade-content-2").style;
-  if (x.maxHeight === "70px" || x.maxHeight === "") {
+  if (x.maxHeight === "65px" || x.maxHeight === "") {
     x.maxHeight = "none";
     document.getElementById('fade-overlay-2').style.display = "none";
     document.getElementById('show-more-button-2').innerText = 'Свернуть';
   } else {
-    x.maxHeight = "70px";
+    x.maxHeight = "65px";
     document.getElementById('fade-overlay-2').style.display = "block";
     document.getElementById('show-more-button-2').innerText = 'Развернуть';
   }
@@ -26,12 +26,12 @@ function showMore2() {
 
 function showMore3() {
   var x = document.getElementById("fade-content-3").style;
-  if (x.maxHeight === "70px" || x.maxHeight === "") {
+  if (x.maxHeight === "65px" || x.maxHeight === "") {
     x.maxHeight = "none";
     document.getElementById('fade-overlay-3').style.display = "none";
     document.getElementById('show-more-button-3').innerText = 'Свернуть';
   } else {
-    x.maxHeight = "70px";
+    x.maxHeight = "65px";
     document.getElementById('fade-overlay-3').style.display = "block";
     document.getElementById('show-more-button-3').innerText = 'Развернуть';
   }
@@ -39,12 +39,12 @@ function showMore3() {
 
 function showMore4() {
   var x = document.getElementById("fade-content-4").style;
-  if (x.maxHeight === "70px" || x.maxHeight === "") {
+  if (x.maxHeight === "65px" || x.maxHeight === "") {
     x.maxHeight = "none";
     document.getElementById('fade-overlay-4').style.display = "none";
     document.getElementById('show-more-button-4').innerText = 'Свернуть';
   } else {
-    x.maxHeight = "70px";
+    x.maxHeight = "65px";
     document.getElementById('fade-overlay-4').style.display = "block";
     document.getElementById('show-more-button-4').innerText = 'Развернуть';
   }
@@ -52,12 +52,12 @@ function showMore4() {
 
 function showMore5() {
   var x = document.getElementById("fade-content-5").style;
-  if (x.maxHeight === "70px" || x.maxHeight === "") {
+  if (x.maxHeight === "65px" || x.maxHeight === "") {
     x.maxHeight = "none";
     document.getElementById('fade-overlay-5').style.display = "none";
     document.getElementById('show-more-button-5').innerText = 'Свернуть';
   } else {
-    x.maxHeight = "70px";
+    x.maxHeight = "65px";
     document.getElementById('fade-overlay-5').style.display = "block";
     document.getElementById('show-more-button-5').innerText = 'Развернуть';
   }
