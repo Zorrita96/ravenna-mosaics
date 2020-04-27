2cea274280c7646240e298be1b64478fb430a1e0
Zorrita <lesyanaz96@gmail.com>
2020-04-27 20:28:28 +0700

    Mobile alignment column. Video lightbox not adopted for mobile.

--- script.js
+++ script.js
@@ -18,42 +18,42 @@ for (lectureBlock of lectureBlocks) {
 =============================*/
   
   function lightbox_open1() {
-    var lightBoxVideo = document.getElementById("video-1"); 
+    let lightBoxVideo = document.getElementById("video-1"); 
     document.getElementById('light-1').style.display = 'block';
     document.getElementById('fade-1').style.display = 'block';
     lightBoxVideo.play();
   }
 
   function lightbox_close1() {
-    var lightBoxVideo = document.getElementById("video-1");
+    let lightBoxVideo = document.getElementById("video-1");
     document.getElementById('light-1').style.display = 'none';
     document.getElementById('fade-1').style.display = 'none';
     lightBoxVideo.pause();
   }
 
   function lightbox_open2() {
-    var lightBoxVideo = document.getElementById("video-2"); 
+    let lightBoxVideo = document.getElementById("video-2"); 
     document.getElementById('light-2').style.display = 'block';
     document.getElementById('fade-2').style.display = 'block';
     lightBoxVideo.play();
   }
 
   function lightbox_close2() {
-    var lightBoxVideo = document.getElementById("video-2");
+    let lightBoxVideo = document.getElementById("video-2");
     document.getElementById('light-2').style.display = 'none';
     document.getElementById('fade-2').style.display = 'none';
     lightBoxVideo.pause();
   }
 
   function lightbox_open3() {
-    var lightBoxVideo = document.getElementById("video-3"); 
+    let lightBoxVideo = document.getElementById("video-3"); 
     document.getElementById('light-3').style.display = 'block';
     document.getElementById('fade-3').style.display = 'block';
     lightBoxVideo.play();
   }
   
   function lightbox_close3() {
-    var lightBoxVideo = document.getElementById("video-3");
+    let lightBoxVideo = document.getElementById("video-3");
     document.getElementById('light-3').style.display = 'none';
     document.getElementById('fade-3').style.display = 'none';
     lightBoxVideo.pause();
@@ -64,7 +64,7 @@ for (lectureBlock of lectureBlocks) {
 =============================*/
 
 function showMore1() {
-    var x = document.getElementById("fade-content-1").style;
+    let x = document.getElementById("fade-content-1").style;
     if (x.maxHeight === "65px" || x.maxHeight === "") {
       x.maxHeight = "none";
       document.getElementById('fade-overlay-1').style.display = "none";
@@ -77,7 +77,7 @@ function showMore1() {
   }
 
 function showMore2() {
-  var x = document.getElementById("fade-content-2").style;
+  let x = document.getElementById("fade-content-2").style;
   if (x.maxHeight === "65px" || x.maxHeight === "") {
     x.maxHeight = "none";
     document.getElementById('fade-overlay-2').style.display = "none";
@@ -90,7 +90,7 @@ function showMore2() {
 }
 
 function showMore3() {
-  var x = document.getElementById("fade-content-3").style;
+  let x = document.getElementById("fade-content-3").style;
   if (x.maxHeight === "65px" || x.maxHeight === "") {
     x.maxHeight = "none";
     document.getElementById('fade-overlay-3').style.display = "none";
@@ -103,7 +103,7 @@ function showMore3() {
 }
 
 function showMore4() {
-  var x = document.getElementById("fade-content-4").style;
+  let x = document.getElementById("fade-content-4").style;
   if (x.maxHeight === "65px" || x.maxHeight === "") {
     x.maxHeight = "none";
     document.getElementById('fade-overlay-4').style.display = "none";
@@ -116,7 +116,7 @@ function showMore4() {
 }
 
 function showMore5() {
-  var x = document.getElementById("fade-content-5").style;
+  let x = document.getElementById("fade-content-5").style;
   if (x.maxHeight === "65px" || x.maxHeight === "") {
     x.maxHeight = "none";
     document.getElementById('fade-overlay-5').style.display = "none";
