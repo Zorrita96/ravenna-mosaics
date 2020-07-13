187e4becb331217b34d74e29a48146a8193d5386
Zorrita <lesyanaz96@gmail.com>
2020-07-13 18:58:19 +0700

    Sidebar added to all html pages. Classes names set to BEM. Sidebar menu items names translated.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+/*=============================
+            SIDEBAR
+=============================*/
+
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
- /* document.getElementById("main").style.marginLeft = "250px"; */
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
@@ -9,6 +12,11 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
 }
 
+
+
+/*=============================
+        LECTURES EMPTY
+=============================*/
 
 // Find all the .lecture-block <divs>
 const lectureBlocks = document.querySelectorAll(".lecture-block");
